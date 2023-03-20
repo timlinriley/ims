9ad6932c9e437132d9ea0fd3a30c9ddfaf099474
Riley Timlin <RTimlin@allseasonmovers.com>
2023-03-20 13:59:56 -0400

    added handlechange to form

--- src/FormDesc.jsx
+++ src/FormDesc.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
+import {useState} from 'react';
 
 const FormDesc = (props) => {
+    const [formData, setFormData] =useState({searchterm:"",});
+
+    const handleChange = (event) => {
+        //use the event object to detect key and value to update
+        setFormData({ ...formData, [event.target.name]: event.target.value });
+        console.log(formData)
+    };
   return (
     <div>
          <form >
         <input 
         type="text" 
         name="searchterm" 
-        // onChange={handleChange} 
-        // value={formData.searchterm}
+        onChange={handleChange} 
+        value={formData.searchterm}
         />
         </form>
     </div>
