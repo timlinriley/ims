2efc65876b05b24ae0d72c3eb7b2992d77ebf4a8
Riley Timlin <RTimlin@allseasonmovers.com>
2023-03-23 14:34:10 -0400

    created order object literal and state for orders

--- src/components/OrderModal.jsx
+++ src/components/OrderModal.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
+import { useState } from 'react';
 
 const OrderModal = (props,{onClose}) => {
     const products = props.products;
+    const emptyOrder = {
+        orderNo: '',
+        product: '',
+        quantity: '',
+
+    };
+    const [order, setOrder] = useState(emptyOrder);
     console.log(products)
+    const handleSubmit = () => {
+
+    }
   return (
     <div>
         
-        <input type="text" placeholder='Order #'></input>
-        <input type="text" placeholder='Product'></input>
-        <input type="text" placeholder='quantity'></input>
+        <input type="text" placeholder='Order #' value={order.orderNo}></input>
+        <input type="text" placeholder='Product' value={order.product}></input>
+        <input type="text" placeholder='quantity' value={order.quantity}></input>
         <button >Submit Order</button>
         <button onClick={onClose}>Close</button>    
     </div>
