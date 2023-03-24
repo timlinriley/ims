24b00b5f5da989b3c32476b45061c6ffcce3c9c5
Riley Timlin <RTimlin@allseasonmovers.com>
2023-03-24 11:04:41 -0400

    added select to create order form

--- src/components/OrderModal.jsx
+++ src/components/OrderModal.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react';
+import Select from 'react-select';
 
 const OrderModal = (props,{onClose}) => {
     const products = props.products;
+    const productOptions = [
+        {
+        label:'',
+        value:'',
+    }
+];
+    const [options, setOptions] = useState(productOptions)
+    // const productOptions = products.map((element) =>{
+    //     return element.title;
+    // })
+    const handleOptions = () => {
+      setOptions(products.map((element) => {
+     return {label: element.title, value: element.title}
+    }))
+    }
+    useEffect(() => {
+        handleOptions();
+    }, [])
     const emptyOrder = {
         orderNo: '',
         product: '',
@@ -15,17 +34,19 @@ const OrderModal = (props,{onClose}) => {
     const handleChange = (e) => {
         const {name, value} = e.target;
         setOrder({...order, [name]: value,})
-        console.log(order)
+        // console.log(order)
     }
     const handleSubmit = (e) => {
         e.preventDefault();
-        setOrders(order)
+        setOrders(...orders, order)
         console.log(orders, '*')
+        console.log(order)
     }
   return (
     <div>
         <form onSubmit={handleSubmit}>
         <input type="text" placeholder='Order #' value={order.orderNo} onChange={handleChange} name="orderNo"></input>
+        <Select name='product' options={options} />
         <input type="text" placeholder='Product' value={order.product} onChange={handleChange} name="product"></input>
         <input type="text" placeholder='quantity' value={order.quantity} onChange={handleChange} name="quantity"></input>
         <input type="submit" value="submit" />
