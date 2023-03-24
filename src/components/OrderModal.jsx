66eef394bd8c6ea2fc14f0095cff502efed4ae75
Riley Timlin <RTimlin@allseasonmovers.com>
2023-03-24 14:26:56 -0400

    fixed select input so that it order is capturing product data

--- src/components/OrderModal.jsx
+++ src/components/OrderModal.jsx
@@ -10,6 +10,8 @@ const OrderModal = (props,{onClose}) => {
         value:'',
     }
 ];
+
+// document.querySelector('.css-b62m3t-container')?.addEventListener('click', (e) => {handleChange(e) } )
     const [options, setOptions] = useState(productOptions)
     // const productOptions = products.map((element) =>{
     //     return element.title;
@@ -30,23 +32,33 @@ const OrderModal = (props,{onClose}) => {
     };
     const [order, setOrder] = useState(emptyOrder);
     const [orders, setOrders] = useState([])
+    const [select, setSelect] =useState();
     console.log(products)
     const handleChange = (e) => {
         const {name, value} = e.target;
+        // const name = e.target.textContent
         setOrder({...order, [name]: value,})
-        // console.log(order)
+        // setSelect(e.target.value)
+        // console.log(name, value)
     }
     const handleSubmit = (e) => {
         e.preventDefault();
-        setOrders(...orders, order)
-        console.log(orders, '*')
-        console.log(order)
+         console.log(order)
+        // setOrders(...orders, order)
+        // console.log(orders, '*')
+        // console.log(order)
     }
   return (
     <div>
         <form onSubmit={handleSubmit}>
         <input type="text" placeholder='Order #' value={order.orderNo} onChange={handleChange} name="orderNo"></input>
-        <Select name='product' options={options} />
+        <select value={select} name="product" onChange={handleChange} >
+            {products.map((element)=> {
+                return (<option name="product" >{element.title}</option> 
+                    )
+            })}
+        </select>
+        {/* <Select name="product" value={options.value} options={options}  /> */}
         <input type="text" placeholder='quantity' value={order.quantity} onChange={handleChange} name="quantity"></input>
         <input type="submit" value="submit" />
         <button onClick={onClose}>Close</button>  
